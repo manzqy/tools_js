662c14ecda8b6afe1937932c81bc807692888c10
xiaoYu <18839199638@163.com>
2019-11-09 20:17:59 +0800

    try more

---
+++ tools.js
@@ -0,0 +1,28 @@
+/* 
+ * author: xiaoYU
+ * Copyright 2019
+ */
+var jTools = {};
+/* ========== randomInt */
+jTools.randomInt = function(n, m) {
+    n = n || 0;
+    m = m || 1;
+    return Math.floor(Math.random() * (m - n + 1));
+}
+/* ========== randomRGB */
+jTools.randomRGB = function() {
+    var r = jTools.randomInt(0,255);
+    var g = jTools.randomInt(0,255);
+    var b = jTools.randomInt(0,255);
+    return "rgb("+ r +","+ g +","+ b +")";
+}
+/* ========== randomHexColor */
+jTools.randomHexColor = function() {
+    var r = jTools.randomInt(0,255).toString(16);
+    var g = jTools.randomInt(0,255).toString(16);
+    var b = jTools.randomInt(0,255).toString(16);
+    return "#"+ r + g + b;
+}
+
+
+
